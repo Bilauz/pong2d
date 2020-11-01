173254e851510785f9a254634925bdf3963ce91d
Vinícius Dias Aresi <contatovdaresi@gmail.com>
2020-11-01 19:59:12 -0300

    Atualização, Movimentação da Bola

---
+++ Pong 2D/Assets/Scripts/Bolinha.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolinha : MonoBehaviour
+{
+    public float velocidadeDaBola;
+
+    public Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig.velocity = new Vector2(velocidadeDaBola, velocidadeDaBola) * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
