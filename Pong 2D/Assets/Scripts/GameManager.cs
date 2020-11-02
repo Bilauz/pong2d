7614cfc9d6769de09a752240ee55fd766bb78b5e
Vinícius Dias Aresi <contatovdaresi@gmail.com>
2020-11-02 00:54:11 -0300

    Atualizaçao, sistema de pontuação dos jogadores

---
+++ Pong 2D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int pontuacaoJogador1;
+
+    public int pontuacaoJogador2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AumentarPontuacao1()
+    {
+        pontuacaoJogador1 += 1;
+    }
+
+    public void AumentarPontuacao2()
+    {
+        pontuacaoJogador2 += 1;
+    }
+
+}
